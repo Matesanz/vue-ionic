db5eebe1794c477adbfcb65e9fd9d04be61632c2
Matesanz <Matesanz.Cuadrado@gmail.com>
2020-10-17 10:53:01 +0200

    Delete Tabs

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <ExploreContainer :name="welcome" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage } from '@ionic/vue';
+import { ellipse, square, triangle, accessibilityOutline } from 'ionicons/icons';
+
+export default {
+  name: 'Home',
+  components: {IonPage, ExploreContainer },
+  data() {
+    return {
+        welcome: "¡Bienvenido Oleg!",
+    }
+  },
+  setup() {
+    return {
+      ellipse, 
+      square, 
+      triangle,
+      accessibilityOutline
+    }
+  }
+}
+</script>
